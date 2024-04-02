82e76ce63574f99c7b152e2a1df995236f601fcc
Dombinik <dominic@Dom>
2024-04-02 16:39:38 +0800

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="bg-gray-950">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center ml-6">
+          <img src="/assets/logo.png" alt="Logo" class="h-24 w-auto ml-6">
+          <h1 class="text-blue-500 font-bold text-xl">Sea<span class="text-white">versity</span></h1>
+        </div>
+        
+
+        <ul class="flex items-center space-x-12 mr-8">
+          <li>
+          <a href="/" class="text-white hover:text-gray-200">Home</a>
+        </li>
+        <li>
+          <a href="/about" class="text-white hover:text-gray-200">About</a>
+        </li>
+        <li>
+          <a href="/services" class="text-white hover:text-gray-200">Services</a>
+        </li>
+        <li>
+          <a href="/contact" class="text-white hover:text-gray-200">Contact</a>
+        </li>
+          <Icon name="uil:bell" color="white" class="flex items-center mr-2 h-12 w-auto"></Icon>
+          
+        </ul>
+      </div>
+    </nav>
+  </template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <Navbar/>
+    <div class="flex justify-center">
+      <div class="flex-grow flex items-center h-28 w-auto m-36">
+        <img src="/assets/logo.png" alt="Your Image" class="">
+      </div>
+  
+      <div class="flex-grow flex items-center mr-52">
+        <form class="max-w-md w-full">
+          <div class="mb-4">
+            <label for="email" class="block text-gray-700 font-bold mb-2">Email</label>
+            <input type="email" id="email" name="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          </div>
+          <div class="mb-6">
+            <label for="password" class="block text-gray-700 font-bold mb-2">Password</label>
+            <input type="password" id="password" name="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            <span class="pr-24 ml-2">send otp</span> 
+            <span class="ml-28">forgot password</span>
+          </div>
+          
+          <div class="flex items-center justify-between">
+            <button type="button" class="bg-white shadow appearance-none border rounded hover:bg-white-700 text-orange-400 font-bold w-full py-2 px-3 rounded focus:outline-none focus:shadow-outline">
+              Log In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
