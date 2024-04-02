fffafe3cbaf684e62b096af5cf520b5824fa00e8
Dombinik <dombitv69@gmail.com>
2024-04-02 21:01:11 +0800

    responsive

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,35 @@
 <template>
-    <nav class="bg-gray-950">
-      <div class="flex items-center justify-between">
-        <div class="flex items-center ml-6">
-          <img src="/assets/logo.png" alt="Logo" class="h-24 w-auto ml-6">
-          <h1 class="text-blue-500 font-bold text-xl">Sea<span class="text-white">versity</span></h1>
-        </div>
-        
+  <nav class="bg-gray-950">
+    <div class="flex items-center justify-between px-6 py-4 md:px-10 lg:px-16 xl:px-20">
+      <div class="flex items-center">
+        <img src="/assets/logo.png" alt="Logo" class="h-12 md:h-16 w-auto">
+        <h1 class="text-blue-500 font-bold text-xl md:text-2xl ml-2 md:ml-4">Sea<span class="text-white">versity</span></h1>
+      </div>
 
-        <ul class="flex items-center space-x-12 mr-8">
-          <li>
+      <div class="md:hidden relative">
+        <button @click="toggleDropdown" class="text-white hover:text-gray-200 focus:outline-none">
+          <Icon name="uil:bars" color="white" class="h-12 w-auto"></Icon>
+        </button>
+        <transition name="fade">
+          <ul v-show="isDropdownOpen" class="absolute top-16 right-0 bg-gray-950 z-10 border border-gray-700 rounded mt-2">
+            <li>
+              <a href="/" class="block py-2 px-4 text-white hover:text-gray-200">Home</a>
+            </li>
+            <li>
+              <a href="/about" class="block py-2 px-4 text-white hover:text-gray-200">About</a>
+            </li>
+            <li>
+              <a href="/services" class="block py-2 px-4 text-white hover:text-gray-200">Services</a>
+            </li>
+            <li>
+              <a href="/contact" class="block py-2 px-4 text-white hover:text-gray-200">Contact</a>
+            </li>
+          </ul>
+        </transition>
+      </div>
+
+      <ul class="hidden md:flex items-center space-x-8">
+        <li>
           <a href="/" class="text-white hover:text-gray-200">Home</a>
         </li>
         <li>
@@ -20,10 +41,17 @@
         <li>
           <a href="/contact" class="text-white hover:text-gray-200">Contact</a>
         </li>
-          <Icon name="uil:bell" color="white" class="flex items-center mr-2 h-12 w-auto"></Icon>
-          tests
-        </ul>
-      </div>
-    </nav>
-  </template>
-  
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isDropdownOpen = ref(false);
+
+function toggleDropdown() {
+  isDropdownOpen.value = !isDropdownOpen.value;
+}
+</script>
