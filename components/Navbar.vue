1c4974af855a9923232cdc4c6fe4dbe05c5dea12
Dombinik <dombitv69@gmail.com>
2024-04-05 10:42:03 +0800

    update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -12,10 +12,10 @@ function toggleDropdown() {
   <nav class="bg-gray-950">
     <div class="flex items-center justify-between px-6 py-4 md:px-10 lg:px-16 xl:px-20">
       <div class="flex items-center">
-        <a href="/" class="flex items-center">
+        <nuxt-link to="/" class="flex items-center">
           <img src="/assets/logo.png" alt="Logo" class="h-12 md:h-16 w-auto">
           <h1 class="text-blue-500 font-bold text-xl md:text-2xl ml-2 md:ml-4">Sea<span class="text-white">versity</span></h1>
-        </a>
+        </nuxt-link>
       </div>
 
       <div class="md:hidden relative">
@@ -25,23 +25,23 @@ function toggleDropdown() {
         <transition name="fade">
           <ul v-show="isDropdownOpen" class="absolute top-16 right-0 bg-gray-950 z-10 border border-gray-700 rounded mt-2">
             <li>
-              <a href="/" class="block py-2 px-4 text-white hover:text-gray-200">Home</a>
+              <nuxt-link to="/" class="block py-2 px-4 text-white hover:text-gray-200">Home</nuxt-link>
             </li>
             <li>
-              <a href="/about" class="block py-2 px-4 text-white hover:text-gray-200">About</a>
+              <nuxt-link to="/about" class="block py-2 px-4 text-white hover:text-gray-200">About</nuxt-link>
             </li>
             <li>
-              <a href="/services" class="block py-2 px-4 text-white hover:text-gray-200">Services</a>
+              <nuxt-link to="/services" class="block py-2 px-4 text-white hover:text-gray-200">Services</nuxt-link>
             </li>
             <li>
-              <a href="/contact" class="block py-2 px-4 text-white hover:text-gray-200">Contact</a>
+              <nuxt-link to="/contact" class="block py-2 px-4 text-white hover:text-gray-200">Contact</nuxt-link>
             </li>
             <li>
-              <router-link to="signuppage" class="flex text-white">
+              <nuxt-link to="signuppage" class="flex text-white">
                 <v-btn>
                   Sign Up
                 </v-btn>
-              </router-link>
+              </nuxt-link>
             </li>
           </ul>
         </transition>
@@ -49,27 +49,25 @@ function toggleDropdown() {
 
       <ul class="hidden md:flex items-center space-x-8">
         <li>
-          <a href="/" class="text-white hover:text-gray-200">Home</a>
+          <nuxt-link to="/" class="text-white hover:text-gray-200">Home</nuxt-link>
         </li>
         <li>
-          <a href="/about" class="text-white hover:text-gray-200">About</a>
+          <nuxt-link to="/about" class="text-white hover:text-gray-200">About</nuxt-link>
         </li>
         <li>
-          <a href="/services" class="text-white hover:text-gray-200">Services</a>
+          <nuxt-link to="/services" class="text-white hover:text-gray-200">Services</nuxt-link>
         </li>
         <li>
-          <a href="/contact" class="text-white hover:text-gray-200">Contact</a>
+          <nuxt-link to="/contact" class="text-white hover:text-gray-200">Contact</nuxt-link>
         </li>
         <li>
-          <router-link to="signuppage" class="flex text-white">
+          <nuxt-link to="signuppage" class="flex text-white">
             <v-btn>
               Sign Up
             </v-btn>
-          </router-link>
+          </nuxt-link>
         </li>
       </ul>
     </div>
   </nav>
 </template>
-
-
